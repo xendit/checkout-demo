755ce2e9106c8dd71a0871ed870db2e58122e6ba
Wildan X1 <wildansnahar@gmail.com>
2020-10-07 15:16:57 +0700

    feat: add required fields

--- server/routes.js
+++ server/routes.js
@@ -29,8 +29,20 @@ router.get('/api/healthcheck/readiness', (req, res) => {
 });
 
 router.post('/api/invoice', async (req, res) => {
+    // generate random external_id
+    const now = new Date();
+
     try {
-        const invoice = await invoiceController.create(req.body);
+        const data = {
+            ...config.invoiceData,
+            external_id: `checkout-demo-${now.getTime()}`,
+            currency: req.body.currency,
+            amount: req.body.amount,
+            failure_redirect_url: req.body.redirect_url,
+            success_redirect_url: req.body.redirect_url
+        };
+
+        const invoice = await invoiceController.create(data);
         return res.status(200).send(invoice.data);
     } catch (e) {
         return res.status(e.response.status).send(e.response.data);
